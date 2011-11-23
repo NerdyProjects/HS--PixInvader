5092b058f7110c2e62753dbc1bfd1d901bbec661
Matthias Larisch <mail@matthias-larisch.de>
2011-11-24 00:19:58 +0100

    timer0 for sound, timer2 for display. just some initialisation code with a lot work to do

--- src/display.c
+++ src/display.c
@@ -16,13 +16,34 @@ xdata unsigned char * data DisplayWrite;
 
 /* IRQ rate: F_OSC / 12 / (256 - RELOAD) */
 #define TIMER0_RELOAD 0		/* 7812,5 Hz */
-/* F_OSC / 12 / (256 - RELOAD) / DISPLAY_DIVIDER */
-#define DISPLAY_DIVIDER 16 /* 488,28 Hz -> ~ 2 ms */
+
+/* we want about 2 ms ~ 500 Hz.
+ * F = F_OSC / 2 / (65536 - RCAP2 HL)
+ * 65536 - RCAP = F_OSC / F / 2
+ * RCAP = -F_OSC / F / 2 + 65536
+ * -24000000 / 500 / 2 + 65536
+ * */
+#define DISPLAY_REFRESH_RATE 500
+
+#ifndef F_OSC
+#define F_OSC 24000000
+#endif
+
+#define DISPLAY_TIMER_RELOAD ((-F_OSC / DISPLAY_REFRESH_RATE / 2) + 65536)
 
 #define DISPLAY_SELECT_OFF (0xF8)
 
 
-static void refreshDisplay(void)
+/* timer 1 ISR.
+ * This serves display and gameplay timer.
+ * Call frequency will be F_OSC / 12 / (65536 - RCAP2 HL)
+ *
+ */
+#ifdef SDCC
+void timer1_isr(void) __interrupt (5) __using (2)
+#else
+void timer1_isr(void)
+#endif
 {
 	static data unsigned char col = 0;
 	data unsigned char i;
@@ -32,45 +53,46 @@ static void refreshDisplay(void)
 		DisplayDataReg = DisplayRead[DISPLAY_COLS_PER_MATRIX*i + col];
 	}
 	DisplaySelectReg = col << 3;
+	if(++col > 7)
+	{
+		col = 0;
+		/* todo set flag for frame completion, handle grayscale buffer change */
+	}
 
 }
 
 
-/* timer 0 ISR. todo: register bank 0 may be changed.
- * this ISR serves the sound, display and gameplay timer.
+/* timer 0 ISR.
+ * this ISR serves the sound timer.
  * Call frequency will be F_OSC / 12 / (256 - TH0).
  * F_OSC of 24 MHz leads to 7812 Hz .
- * Division of 16 for display leads to 48,83 Hz picture refresh rate (2 gray steps, 5 columns)
- *
  * total execution time must not exceed timer rate to prevent audio jitter!
- * added latency every 1/DISPLAY_DIVIDER sample will be audible!
  * */
 #ifdef SDCC
-void timer0_isr(void) __interrupt (1) __using (0)
+void timer0_isr(void) __interrupt (1) __using (1)
 #else
 void timer0_isr(void)
 #endif
 {
-	static data unsigned char displayDivider = DISPLAY_DIVIDER;
-
-	/* sound output has to be done first to prevent jitter */
-
-
-	if(--displayDivider == 0)
-	{
-		displayDivider = DISPLAY_DIVIDER;
-		refreshDisplay();
-	}
 
 }
 
 void main(void)
 {
-	TMOD = M1_0;
+	TMOD = M1_0;	/* 8 bit timer */
 	TH0 = TIMER0_RELOAD;
 	TR0 = 1;
+	ET0 = 1;
+	PT0 = 1;		/* high priority IRQ */
+
+	RCAP2H = DISPLAY_TIMER_RELOAD >> 8;
+	RCAP2L = DISPLAY_TIMER_RELOAD;
+	TR2 = 1;
+	ET2 = 1;
+
 	DisplaySelectReg = 0;
 	DisplayDataReg = 2;
+	EA = 1;
 	while(1)
 		;
 }
