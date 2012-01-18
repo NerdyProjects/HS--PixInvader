fde7b176f003d30454cd876fc298be0f50c9f2e1
Matthias Larisch <mail@matthias-larisch.de>
2012-01-18 19:19:06 +0100

    cleanup of main. add SPI. still display testcode

--- src/main.c
+++ src/main.c
@@ -3,17 +3,40 @@
 #include "sound.h"
 #include "keys.h"
 #include "pixinvaders.h"
+#include "writeEeprom.h"
+
+
+data volatile unsigned long waitVar;
+void waitFewMs(void)
+{
+	waitVar = 0;
+	while(waitVar < 2000UL)
+		waitVar++;
+}
+
 
 void main(void)
 {
+	unsigned char x, y;
+	unsigned char correctRead = 1;
 	PAGE_SELECT = PDATA_PAGE;		/* not really neccessary, but so everything is clear... */
-	keyInit();
+
+	//keyInit();
 	displayInit();
-	soundInit();
+	/*soundInit(); */
 	EA = 1;
+
 	while(1)
 	{
-		game();
+		handleSPI();
+		for(x = 0; x < 20; ++x)
+		{
+			for(y = 0; y < 14; ++y)
+			{
+				displayPixel(x, y, COLOR_FULL);
+				displayChangeBuffer();
+				waitFewMs();
+			}
+		}
 	}
-
 }
