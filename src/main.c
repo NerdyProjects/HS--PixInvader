b6a56ad86a08b40725a205a52672d0c7a05768f5
Matthias Larisch <mail@matthias-larisch.de>
2012-01-25 00:45:28 +0100

    make SPI work. additionally, add READ_MULTI command (untested).

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include "sound.h"
 #include "keys.h"
 #include "pixinvaders.h"
-#include "writeEeprom.h"
+#include "font.h"
+#include "spi_command.h"
 
 
 data volatile unsigned long waitVar;
@@ -14,22 +15,56 @@ void waitFewMs(void)
 		waitVar++;
 }
 
+void waitALittleSecond(unsigned char wait)
+{
+  unsigned char i;
+  for(i = 0; i < wait; ++i)
+	  waitFewMs();
+}
 
 void main(void)
 {
-	unsigned char x, y;
-	unsigned char correctRead = 1;
+	unsigned char i;
 	PAGE_SELECT = PDATA_PAGE;		/* not really neccessary, but so everything is clear... */
 
 	//keyInit();
 	displayInit();
-	/*soundInit(); */
+	soundInit();
 	EA = 1;
+/*
+	//displayNumber(0, 0, SampleInfo[0].length >> 8);		// 100
+	//displayNumber(0, 0, SampleInfo[0].loopEntry);			// 204
+	for(i = 0; i < 12; ++i)
+	{
+		displayNumber(0, 0, i);
+		displayChangeBuffer();
+		playSample(i, 0, 20);
+		waitALittleSecond(15);
+	}
 
+	displayString(0, 0, "Song", LINEBREAK_NONE);
+	displayChangeBuffer();
+	stopSong();
+
+	playSong(0);
+
+	waitALittleSecond(200);
+	waitALittleSecond(200);
+	waitALittleSecond(200);
+	stopSong();
+	displayString(0, 4, "Stop", LINEBREAK_NONE);
+	displayChangeBuffer();
+	*/
+	displayString(0, 0, "Dies ist ein Text!", LINEBREAK_X0);
+	displayChangeBuffer();
 	while(1)
 	{
 		handleSPI();
-		for(x = 0; x < 20; ++x)
+		LED_OFF();
+		EA = 1;
+
+		waitALittleSecond(20);
+		/*for(x = 0; x < 20; ++x)
 		{
 			for(y = 0; y < 14; ++y)
 			{
@@ -37,6 +72,6 @@ void main(void)
 				displayChangeBuffer();
 				waitFewMs();
 			}
-		}
+		} */
 	}
 }
--- src/spi.h
+++ src/spi.h
@@ -22,10 +22,16 @@
 #define OP_WRITE_MULTI 0x03		/* ADDR HIGH, ADDR LOW, SIZE (SIZE up to 64 byte)*/
 #define OP_MULTI_DATA 0x80		/* 3x DATA; unused data will be discarded */
 #define OP_FINISHED 0xF0		/* leave SPI mode */
+#define OP_ERROR	0xFF		/* signal error in SPI handling. used as internal command */
+#define OP_DISABLE_SDP 0xF1		/* send DISABLE SDP command to EEPROM at 0x8000 */
 #define OP_READ_XDATA 0x04		/* read byte of xdata memory: ADDR HIGH, ADDR LOW, DATA(in)*/
 #define OP_READ_DATA 0x05		/* read byte of data memory: ADDR HIGH (0), ADDR LOW, DATA(in)*/
+#define OP_READ_MULTI 0x08		/* read multiple bytes from xdata memory. ADDR HIGH, LOW, COUNT.
+ 	 	 	 	 	 	 	 	   gets followed by OP_MULTI_DATA (but read access here)*/
 /* OP-CODES that have bit 2 (0x04) set are special cases for read access!
- * do not define another one without changing handling ASM code! */
+ * do not define another one without changing handling ASM code!
+ * OP-CODES that have bit 3 (0x08) set are also special cases!
+ * do not define those! */
 
 
 #endif /* SPI_H_ */
